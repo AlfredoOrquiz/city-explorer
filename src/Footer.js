6dd037f46da2b5249187cfb20cf3db8fbe25cf46
Alfredo <fredy_rios0123@outlook.com>
2022-08-19 11:41:24 -0700

    Started City explorer

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'ract';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <>
+        <footer>
+          
+        </footer>
+      </>
+    )
+  }
+}
+
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,42 @@
+import React from 'ract';
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    city: ''
+  }
+  }
+
+  handleCity = (e) => {
+    this.setState({
+      city: e.target.city.value
+    });
+    console.log(this.state.city);
+    // Get data from some API's
+  }
+
+  submitForm = (e) => {
+    e.preventDefault();
+
+    console.log(this.state.city);
+    // Get data from API's
+  }
+  render() {
+    return (
+      <>
+        <form onSubmit={this.submitForm}>
+          <label>Pick a City
+            <input 
+            type='text' 
+            name='city' 
+            onInput={this.handleCity}/>
+            <button>Submit</button>
+          </label>
+        </form>
+      </>
+    )
+  }
+}
+
+export default Main;
