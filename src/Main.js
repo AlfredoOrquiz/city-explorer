651e38aac2d6aa56868f7cd7930c2ab349097eb8
Alfredo <fredy_rios0123@outlook.com>
2022-08-23 20:22:52 -0700

    Finished checklist #4

--- src/Main.js
+++ src/Main.js
@@ -22,18 +22,27 @@ class Main extends React.Component {
             <Button type="submit" variant="primary">Explore!</Button>{' '}
           </Form.Group>
         </Form>
-
-        <Card>
-          <Card.Body>
-            <Card.Title>{this.props.cityData.display_name}</Card.Title>
-            <Card.Img variant="top" src={this.props.mapURL}/>
-            <Card.Text></Card.Text>
-          </Card.Body>
-          <ListGroup>
-            <ListGroup.Item>'Lat: {this.props.cityData.lat}'</ListGroup.Item>
-            <ListGroup.Item>'Lon: {this.props.cityData.lon}'</ListGroup.Item>
-          </ListGroup>
-        </Card>
+        {
+          this.props.showCard
+          ?
+            <p>{' '}</p>
+          :
+          this.props.error
+          ?
+            <p>{this.props.errorMessage}</p>
+          :
+          <Card>
+            <Card.Body>
+              <Card.Title>{this.props.cityData.display_name}</Card.Title>
+              <Card.Img variant="top" src={this.props.mapURL}/>
+              <Card.Text></Card.Text>
+            </Card.Body>
+            <ListGroup>
+              <ListGroup.Item>'Lat: {this.props.cityData.lat}'</ListGroup.Item>
+              <ListGroup.Item>'Lon: {this.props.cityData.lon}'</ListGroup.Item>
+            </ListGroup>
+          </Card>
+         }
       </>
     )
   }
